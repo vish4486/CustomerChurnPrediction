80a2de4b32e8f8abd06c64f5fb8934d5bf2f1cad
Vishal Nigam <vishalnigamcusat@gmail.com>
2025-06-25 22:25:30 +0200

    fixing some quality bugs

--- src/evaluate.py
+++ src/evaluate.py
@@ -20,14 +20,12 @@ def display_confusion_matrix(y_true, y_pred, model_name='Model'):
 def plot_model_performance(results):
     df = pd.DataFrame(results)
     metrics = ['Balanced Accuracy', 'F1 Score', 'Precision', 'Recall (TPR)', 'ROC AUC']
-    #plt.figure(figsize=(12, 6))
     for metric in metrics:
         fig = plt.figure(figsize=(12, 6))
         sns.barplot(x='Model', y=metric, data=df)
         plt.title(f'Model Comparison - {metric}')
         plt.xticks(rotation=45)
         plt.tight_layout()
-        #plt.show()
         filename = f"{metric.lower().replace(' ', '_').replace('(', '').replace(')', '').replace('/', '_')}_comparison.png"
         save_plot(fig, filename)
 
--- src/hyperparameter_tuning.py
+++ src/hyperparameter_tuning.py
@@ -55,7 +55,7 @@ def tune_xgboost(X, y):
     scale_pos_weight = counter[0] / counter[1]
 
     grid = GridSearchCV(
-        XGBClassifier(use_label_encoder=False, eval_metric='logloss',random_state=42,scale_pos_weight = scale_pos_weight),
+        XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42, scale_pos_weight=scale_pos_weight),
         param_grid,
         scoring='f1_weighted',
         cv=StratifiedKFold(n_splits=5, shuffle=True, random_state=42),
